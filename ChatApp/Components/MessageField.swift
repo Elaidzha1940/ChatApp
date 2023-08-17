aedaaa16241e507fcf29d89e7aa99688f28204ef
Elaidzha1940 <eli080293@gmail.com>
2023-08-17 18:32:43 +0300

    added MessageField

---
+++ ChatApp/Components/MessageField.swift
@@ -0,0 +1,26 @@
+//  /*
+//
+//  Project: ChatApp
+//  File: MessageField.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct MessageField: View {
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageField_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageField()
+    }
+}
+
