06c15f3b30714f1820949795ba5ad58a2c697cf8
Elaidzha1940 <eli080293@gmail.com>
2023-08-17 17:40:22 +0300

    MessageBubble

--- ChatApp/Components/MessageBubble.swift
+++ ChatApp/Components/MessageBubble.swift
@@ -12,15 +12,28 @@
 import SwiftUI
 
 struct MessageBubble: View {
+    
     var message: Message
+    @State private var showTime = false
+    
     var body: some View {
         
         VStack(alignment: message.received ? .leading : .trailing) {
             
             HStack {
                 Text(message.text)
+                    .padding()
+                    .background(message.received ? Color("violet") : Color("peach"))
+                    .cornerRadius(30)
+            }
+            .frame(maxWidth: 300, alignment: message.received ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
             }
         }
+        .frame(maxWidth: .infinity, alignment: message.received ? .leading : .trailing)
+        .padding(message.received ? .leading : .trailing)
+        .padding(.horizontal, 10)
     }
 }
 
