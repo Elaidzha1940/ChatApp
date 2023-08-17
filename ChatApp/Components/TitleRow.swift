c2f4335a89bcdcf7165b7601123679c54cf89ba7
Elaidzha1940 <eli080293@gmail.com>
2023-08-17 15:44:02 +0300

    added Colors and a new File / TitleRow

---
+++ ChatApp/Components/TitleRow.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: ChatApp
+//  File: TitleRow.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.08.2023
+//
+//  Status: in progress | Not decorated
+//
+//  */
+
+import SwiftUI
+
+struct TitleRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleRow()
+    }
+}
