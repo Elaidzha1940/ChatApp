e68bd4a395a34f0cc4f4076b8551b62d8f1ef4d6
Elaidzha1940 <eli080293@gmail.com>
2023-08-17 21:59:26 +0300

    added a new File / MessagesManager

---
+++ ChatApp/MessagesManager.swift
@@ -0,0 +1,12 @@
+//  /*
+//
+//  Project: ChatApp
+//  File: MessagesManager.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.08.2023
+//
+//   Status: in progress | Decorated
+//
+//  */
+
+import Foundation
