410e1e65e08e64c63b2599fc150346cc8ae7fc5e
Elaidzha1940 <eli080293@gmail.com>
2023-08-17 18:50:58 +0300

    MessageField

--- ChatApp/Components/MessageField.swift
+++ ChatApp/Components/MessageField.swift
@@ -12,19 +12,40 @@
 import SwiftUI
 
 struct MessageField: View {
+    
+    @State private var message = ""
+    
     var body: some View {
         
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            CustomTextField(placeholder: Text("Enter your message here..."), text: $message)
+            
+            Button {
+                print("Message sent!")
+                message = ""
+            } label: {
+                Image(systemName: "paperplane.fill")
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color("peach"))
+                    .cornerRadius(40)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .background(Color("violet"))
+        .cornerRadius(50)
+        .padding()
     }
 }
-
 struct MessageField_Previews: PreviewProvider {
     static var previews: some View {
         MessageField()
     }
 }
 
-struct CustomTextField: View {
+struct CustomTextField : View {
     var placeholder: Text
     @Binding var text: String
     var editingChanged: (Bool) -> () = {_ in}
