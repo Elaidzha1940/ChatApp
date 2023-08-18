c9980cd863b8903c68ccae6b787c880488493011
Elaidzha1940 <eli080293@gmail.com>
2023-08-18 13:04:49 +0300

    MessagesManager

--- ChatApp/MessagesManager.swift
+++ ChatApp/MessagesManager.swift
@@ -18,7 +18,26 @@ class MessagesManager: ObservableObject {
    @Published private(set) var messages: [Message] = []
     let db = Firestore.firestore()
     
+    init() {
+        getMessages()
+    }
+    
     func getMessages() {
-        db.collection("messages")
+        db.collection("messages").addSnapshotListener { querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(String(describing: error))")
+                return
+            }
+            
+            documents.compactMap { documents -> Message? in
+                
+                do {
+                    return try documents.data(as: Message.self)
+                } catch {
+                    print("Error decoding document into Message: \(error)")
+                    return nil
+                }
+            }
+        }
     }
 }
